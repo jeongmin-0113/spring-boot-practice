14cb277917daf2e07cdaded27180b46fbf01c8b1
Jeongmin Park <roro.rolling14@gmail.com>
2023-04-28 23:59:06 +0900

    update: 20230428
    점프 투 스프링부트 교재 2-05까지

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 머임요?");
+		q1.setContent("알려주셈ㅜ");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문");
+		q2.setContent("id 자동 생성됨?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 
 }
