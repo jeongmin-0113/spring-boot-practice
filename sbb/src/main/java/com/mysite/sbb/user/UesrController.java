8284d7bc8b697b1f6c9970ee50090e018bf8ee49
Jeongmin Park <roro.rolling14@gmail.com>
2023-05-14 20:04:42 +0900

    update: 20230513
    점프 투 스프링부트 3-07까지

--- sbb/src/main/java/com/mysite/sbb/user/UesrController.java
+++ sbb/src/main/java/com/mysite/sbb/user/UesrController.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +29,23 @@ public class UesrController {
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
-        return "redirect:/";
+        try {
+            userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    private String login() {
+        return "login_form";
     }
 }
