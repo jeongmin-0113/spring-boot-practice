0e337c18bd3502c7b5e10b0adba0fe7f3c02e187
challonsy <101979736+challonsy@users.noreply.github.com>
2023-06-26 17:13:38 +0900

    닉네임 관련 수정

--- sbb/src/main/java/com/mysite/sbb/user/UserService.java
+++ sbb/src/main/java/com/mysite/sbb/user/UserService.java
@@ -14,11 +14,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public SiteUser create(String username, String email, String password) {
+    public SiteUser create(String username, String email, String password, String nickname) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
+        user.setNickname(nickname);
         this.userRepository.save(user);
         return user;
     }
@@ -26,6 +27,12 @@ public class UserService {
     public SiteUser getUser(String username) {
         Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
         if (siteUser.isPresent()) return siteUser.get();
-        else throw new DataNotFoundException("siteuser not found");
+        else throw new DataNotFoundException("siteUser not found");
+    }
+
+    public SiteUser getUserByNickname(String nickname) {
+        Optional<SiteUser> siteUser = this.userRepository.findByNickname(nickname);
+        if (siteUser.isPresent()) {return siteUser.get();}
+        else throw new DataNotFoundException(("siteUser not found"));
     }
 }
